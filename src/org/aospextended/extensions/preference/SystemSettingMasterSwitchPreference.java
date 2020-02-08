syberia-project/platform_packages_apps_SyberiaSettings

preference: Add SystemSettingsMasterSwitchPreference
- Import MasterSwitchPreference from Settings(Lib)
- Add System setting variant + fix initial values
- Use icon_frame as ic in layout instead of icon_container to fix hiding
  icon
- Allow launching fragments with the android:fragment extra same as
  currently possible for PreferenceScreens
- Add possibility to reload value when it might have changed
  (best to call onResume when the value can also be changed from the
  sub preference screen)

@ZeNiXxX: Adapt to AndroidX

Original commit history for TwoTargetPreference from
frameworks/base/packages/SettingsLib:

commit 8a01abf592762c75bb92a9e1787227c44f1f4a8e
Author: Fan Zhang <zhfan@google.com>

    Adjust icon width to match the width with framework

commit 035ff93430b71ef856f299c9f676f8d340403e16
Author: Fan Zhang <zhfan@google.com>

    Move two target preference layout from settings to lib

Original commit history for MasterSwitchPreference.java:

commit e2ad415fdba79b27dfd2c98a2b01d56db9257c71
Author: Doris Ling <dling@google.com>

    Add content description to the master switch toggle button.

commit c68ae0b9fdd5f5a149364d0af7e8a9497a91adfd
Author: Julia Reynolds <juliacr@google.com>

    Channel settings updates

commit aa805e70ed0c7f4eaa66b6e6a0b79bf1b3b9d16d
Author: Doris Ling <dling@google.com>

    Allow settings preference title to span over multiple lines.

commit fe292515d88e803057a5b6077f67ce284b595d7a
Author: Fan Zhang <zhfan@google.com>

    Refactor the layout for MasterSwitch to a 2 target pref

commit 6467d2027e8fefd646af03c08d51d91386843463
Author: Doris Ling <dling@google.com>

    Extends the touch area for the switch in Master Switch preference.

commit 762a733afa26b3d49edfae81536c9fcbe9384483
Author: Julia Reynolds <juliacr@google.com>

    Allow switch preferences to span lines.

commit 56199e94876d4bbd89fb0272156702a321dd681e
Author: Fan Zhang <zhfan@google.com>

    Misc fix on network & internet screen for developer preview

commit c

src/com/syberia/settings/preference/SystemSettingMasterSwitchPreference.java
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.syberia.settings.preference.SystemSettingsStore;
+
+public class SystemSettingMasterSwitchPreference extends MasterSwitchPreference {
+
+    public SystemSettingMasterSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingMasterSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingMasterSwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+}

shivatejapeddi/platform_packages_apps_Extensions

src/org/aospextended/extensions/preference/SystemSettingMasterSwitchPreference.java
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aospextended.extensions.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.aospextended.extensions.preference.SystemSettingsStore;
+
+public class SystemSettingMasterSwitchPreference extends MasterSwitchPreference {
+
+    public SystemSettingMasterSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingMasterSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingMasterSwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+}